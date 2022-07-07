781d0af84d2c2d466c082417b2142f01935f3a88
Caio <caiusito@gmail.com>
2022-07-07 17:10:02 -0300

    Requi 2, corrigindo Lint

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import logo from './logo.svg';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
+    <>
       <BrowserRouter>
-        <Route exact path="/" component={Home}/>
+        <Route exact path="/" component={ Home } />
       </BrowserRouter>
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {/* Learn React */}
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={ logo } className="App-logo" alt="logo" />
+          <p>Edit src/App.js and save to reload.</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+
+    </>
   );
 }
 // olá
